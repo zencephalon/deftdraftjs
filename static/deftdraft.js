51bcc23cec82f1bc525a5df0ff91c3fcbbad0b23
Matthew Bunday <zenceph@gmail.com>
2014-01-25 06:18:29 -0500

    DRRRRRRRRRY

--- static/deftdraft.js
+++ static/deftdraft.js
@@ -2,39 +2,41 @@ function DeftDraft(textarea) {
   this.textarea = textarea;
 }
 
-// ================== next / prev ===================
-
-DeftDraft.prototype.next = function(obj_t) {
-  this.textFunc(obj_t).call(this, function(sel, content) { this.selectForward(sel, content, DeftDraft.regexDict['n'][obj_t]) });
-}
-
-DeftDraft.prototype.prev = function(obj_t) {
-  this.textFunc(obj_t).call(this, function(sel, content) { this.selectBackward(sel, content, DeftDraft.regexDict['p'][obj_t]) });
-}
-
-// ================= text element helpers ================
-
-DeftDraft.prototype.textobject = function(beforeFunc, afterFunc, func) {
-  var content = this.textarea.val();
-  var sel = this.textarea.getSelection();
-
-  var before = beforeFunc.call(this, sel.start, content);
-  var after = afterFunc.call(this, sel.end, content);
-
-  if (this.alreadySelected(before, after)) {
-    func.call(this, sel, content);
-  } else {
-    this.textarea.setSelection(sel.start - before, sel.end + after);
-  }   
+DeftDraft.prototype.command = function(dir, obj_t) {
+  this.textFunc(obj_t).call(this, function(sel, content) { 
+    this.selectFunc(dir, sel, content, DeftDraft.regexDict[dir][obj_t]) });
 }
 
 DeftDraft.prototype.textFunc = function(t_obj) {
   return function(func) {
-    this.textobject(this.boundaryFunc('b', t_obj), this.boundaryFunc('a', t_obj), func);
+    var content = this.textarea.val();
+    var sel = this.textarea.getSelection();
+
+    var before = this.boundaryFunc.call(this, 'b', t_obj)(sel.start, content);
+    var after = this.boundaryFunc.call(this, 'a', t_obj)(sel.end, content);
+
+    if (this.alreadySelected(before, after)) {
+      func.call(this, sel, content);
+    } else {
+      this.textarea.setSelection(sel.start - before, sel.end + after);
+    }
+  }
+}
+
+DeftDraft.prototype.boundaryFunc = function(dir, t_obj) {
+  return function(pos, content) {
+    b = DeftDraft.regexDict[dir][t_obj];
+    pos -= b[0];
+    content = dir == 'a' ? content.substr(pos) : content.substr(0, pos).split("").reverse("").join("");
+    return (res = b[1].exec(content)) !== null ? res.index : content.length;
   }
 }
 
 // =================== select helpers =================
+
+DeftDraft.prototype.selectFunc = function(dir, sel, content, regex) {
+  return dir == 'n' ? this.selectForward(sel, content, regex) : this.selectBackward(sel, content, regex);
+}
 
 DeftDraft.prototype.selectForward = function(sel, content, regex) {
   res = regex.exec(content.substr(sel.end));
@@ -56,17 +58,6 @@ DeftDraft.prototype.selectBackward = function(sel, content, regex) {
     sel.start = sel.end = content.length;
     this.selectBackward(sel, content, regex);
   }
-}
-
-// ==================== boundary helpers ===================
-
-DeftDraft.prototype.returnResult = function(res, content) {
-  return res !== null ? res.index : content.length;
-}
-
-DeftDraft.prototype.boundary = function(dir, pos, content, regex) {
-  content = dir == 'a' ? content.substr(pos) : this.reverse(content.substr(0, pos));
-  return this.returnResult(regex.exec(content), content);
 }
 
 // ======================== boundaries =====================
@@ -94,14 +85,6 @@ DeftDraft.regexDict = {
   }
 }
 
-DeftDraft.prototype.boundaryFunc = function(dir, t_obj) {
-  return function(pos, content) {
-    b = DeftDraft.regexDict[dir][t_obj];
-    pos -= b[0];
-    return this.boundary(dir, pos, content, b[1]);
-  }
-}
-
 // ===================== helpers ==================
 
 DeftDraft.prototype.reverse = function(str) {
@@ -115,9 +98,7 @@ DeftDraft.prototype.alreadySelected = function(start, end) {
 // =================== bindings ===================
 
 var dd = new DeftDraft($('#editor'));
-Mousetrap.bind('ctrl+w', function() {dd.next('w'); return false});
-Mousetrap.bind('ctrl+shift+w', function() {dd.prev('w'); return false});
-Mousetrap.bind('ctrl+s', function() {dd.next('s'); return false});
-Mousetrap.bind('ctrl+shift+s', function() {dd.prev('s'); return false});
-Mousetrap.bind('ctrl+q', function() {dd.next('q'); return false});
-Mousetrap.bind('ctrl+shift+q', function() {dd.prev('q'); return false});
+['w', 's', 'q'].forEach(function (letter) {
+  Mousetrap.bind('ctrl+' + letter, function() {dd.command('n', letter); return false});
+  Mousetrap.bind('ctrl+shift+' + letter, function() {dd.command('p', letter); return false});
+});
