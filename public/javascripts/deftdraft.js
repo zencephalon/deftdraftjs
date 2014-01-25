d3df2abf0d1a0099b6bab7af3441a98404e55e9d
Hasnain <hasnain@a.out>
2014-01-25 04:55:27 -0500

    futile atttempts at insert and delete

--- public/javascripts/deftdraft.js
+++ public/javascripts/deftdraft.js
@@ -2,31 +2,60 @@ function DeftDraft(textarea) {
   this.textarea = textarea;
 }
 
-
-// If we are in a word (no selection or partial selection), select it.
-// If we are selecting a word, select the next word.
-
-DeftDraft.prototype.word = function(func) {
+DeftDraft.prototype.textobject = function(beforeFunc, afterFunc, func) {
   var content = this.textarea.val();
   var sel = this.textarea.getSelection();
 
-  var before = this.wordBoundaryBefore(sel.start, content); // -> position
-  var after = this.wordBoundaryAfter(sel.end, content);
+  var before = beforeFunc.call(this, sel.start, content); // -> position
+  var after = afterFunc.call(this, sel.end, content);
 
+  console.log(before + ", " + after);
   if (this.alreadySelected(before, after)) {
-    func(sel, content);
+    func.call(this, sel, content);
   } else {
     this.textarea.setSelection(sel.start - before, sel.end + after);
-  }  
+  }   
 }
 
+DeftDraft.prototype.word = function(func) {
+  this.textobject(this.wordBoundaryBefore, this.wordBoundaryAfter, func);
+}
+
+DeftDraft.prototype.sentence = function(func) {
+  this.textobject(this.sentenceBoundaryBefore, this.sentenceBoundaryAfter, func);
+}
+
+DeftDraft.prototype.paragraph = function(func) {
+  this.textobject(this.paragraphBoundaryBefore, this.paragraphBoundaryAfter, func);
+}
+
+// ================== next / prev ===================
+
 DeftDraft.prototype.nextWord = function() {
-  that = this; this.word(function(sel, content) {that.selectWordAfter(sel, content)});
+  this.word(this.selectWordAfter);
 }
 
 DeftDraft.prototype.prevWord = function() {
-  that = this; this.word(function(sel, content) {that.selectWordBefore(sel, content)});
+  this.word(this.selectWordBefore);
 }
+
+DeftDraft.prototype.nextSentence = function() {
+  this.sentence(this.selectSentenceAfter);
+}
+
+DeftDraft.prototype.prevSentence = function() {
+  this.sentence(this.selectSentenceBefore);
+}
+
+DeftDraft.prototype.nextParagraph = function() {
+  this.paragraph(this.selectParagraphAfter);
+}
+
+DeftDraft.prototype.prevParagraph = function() {
+  this.paragraph(this.selectParagraphBefore);
+}
+
+// =================== after / before =================
 
 DeftDraft.prototype.selectWordAfter = function(sel, content) {
   content_after = content.substr(sel.end);
@@ -54,9 +83,46 @@ DeftDraft.prototype.selectWordBefore = function(sel, content) {
   }
 }
 
-DeftDraft.prototype.reverse = function(str) {
-  return str.split("").reverse().join("");
+DeftDraft.prototype.selectSentenceAfter = function(sel, content) {
+  sel.end = sel.end + 1;
+  content_after = content.substr(sel.end);
+  res = /.*?[.!?](\W|$)/.exec(content_after);
+
+  if (res !== null) {
+    this.textarea.setSelection(sel.end + res.index, sel.end + res.index + res[0].length - res[1].length);
+  } else {
+    sel.start = 0;
+    sel.end = 0;
+    this.selectSentenceAfter(sel, content);
+  }
 }
+
+DeftDraft.prototype.selectSentenceBefore = function(sel, content) {
+  content_before = this.reverse(content.substr(0, sel.start));
+  res = /(^|\W)[.?!].*?(\W[.!?]|$)/.exec(content_before);
+
+  if (res !== null) {
+    this.textarea.setSelection(sel.start - res.index - res[0].length + res[2].length, sel.start - res.index - res[1].length);
+  } else {
+    sel.start = content.length;
+    sel.end = content.length;
+    this.selectSentenceBefore(sel, content);
+  }
+}
+
+DeftDraft.prototype.selectParagraphAfter = function(sel, content) {
+  sel.end = sel.end + 1;
+  content_after = content.substr(sel.end);
+  res = /.*?(\n\n|$)/.exec(content_after);
+
+  console.log(res);
+
+  if (res !== null) {
+    this.textarea.setSelection(sel.end + res.index, sel.end + res.index + res[0].length - res[1].length);
+  }
+}
+
+// ==================== boundaries ===================
 
 DeftDraft.prototype.wordBoundaryBefore = function(pos, content) {
   content = this.reverse(content.substr(0, pos));
@@ -80,28 +146,9 @@ DeftDraft.prototype.wordBoundaryAfter = function(pos, content) {
   }
 }
 
-DeftDraft.prototype.alreadySelected = function(start, end) {
-  return (start === 0 && end === 0);
-}
-
-DeftDraft.prototype.sentence = function(func) {
-  var content = this.textarea.val();
-  var sel = this.textarea.getSelection();
-
-  var before = this.sentenceBoundaryBefore(sel.start, content); // -> position
-  var after = this.sentenceBoundaryAfter(sel.end, content);
-
-  console.log(before + ", " + after);
-  if (this.alreadySelected(before, after)) {
-    func(sel, content);
-  } else {
-    this.textarea.setSelection(sel.start - before, sel.end + after);
-  }   
-}
-
 DeftDraft.prototype.sentenceBoundaryBefore = function(pos, content) {
   content = this.reverse(content.substr(0, pos));
-  res = /\W[.!?]/.exec(content);
+  res = /(^|\W)[.!?]/.exec(content);
 
   if (res !== null) {
     return res.index;
@@ -111,8 +158,9 @@ DeftDraft.prototype.sentenceBoundaryBefore = function(pos, content) {
 }
 
 DeftDraft.prototype.sentenceBoundaryAfter = function(pos, content) {
+  pos = pos - 1;
   content = content.substr(pos);
-  res = /[.!?]\W/.exec(content);
+  res = /[.!?](\W|$)/.exec(content);
 
   if (res !== null) {
     return res.index;
@@ -121,47 +169,36 @@ DeftDraft.prototype.sentenceBoundaryAfter = function(pos, content) {
   }
 }
 
-DeftDraft.prototype.nextSentence = function() {
-  that = this; this.sentence(function(sel, content) {that.selectSentenceAfter(sel, content)});
-}
+DeftDraft.prototype.paragraphBoundaryBefore = function(pos, content) {
+  content = this.reverse(content.substr(0, pos));
+  res = /\n\n/.exec(content);
 
-DeftDraft.prototype.prevSentence = function() {
-  that = this; this.sentence(function(sel, content) {that.selectSentenceBefore(sel, content)});
-}
-
-DeftDraft.prototype.selectSentenceAfter = function(sel, content) {
-  content_after = content.substr(sel.end);
-  res = /.*?[.!?]\W/.exec(content_after);
-
-  console.log(res);
   if (res !== null) {
-    this.textarea.setSelection(sel.end + res.index, sel.end + res.index + res[0].length);
+    return res.index;
   } else {
-    sel.start = 0;
-    sel.end = 0;
-    this.selectSentenceAfter(sel, content);
+    return content.length;
+  }  
+}
+
+DeftDraft.prototype.paragraphBoundaryAfter = function(pos, content) {
+  content = content.substr(pos);
+  res = /\n\n/.exec(content);
+
+  if (res !== null) {
+    return res.index;
+  } else {
+    return content.length;
   }
 }
 
-DeftDraft.prototype.selectSentenceBefore = function(sel, content) {
-  content_before = this.reverse(content.substr(0, sel.start));
-  res = /\W[.!?].*?\W/.exec(content_before);
+// ===================== helpers ==================
 
-  if (res !== null) {
-    this.textarea.setSelection(sel.start - res.index - res[0].length, sel.start - res.index);
-  } else {
-    sel.start = content.length;
-    sel.end = content.length;
-    this.selectSentenceBefore(sel, content);
-  }
+DeftDraft.prototype.reverse = function(str) {
+  return str.split("").reverse().join("");
 }
 
-DeftDraft.prototype.nextParagraph = function() {
-
-}
-
-DeftDraft.prototype.prevParagraph = function() {
-
+DeftDraft.prototype.alreadySelected = function(start, end) {
+  return (start === 0 && end === 0);
 }
 
 DeftDraft.prototype.nextHeading = function() {
@@ -193,9 +230,6 @@ Mousetrap.bind('ctrl+e', function() {dd.expand(); return false});
 Mousetrap.bind('ctrl+shift+e', function() {dd.compress(); return false});
 
 document.getElementById("editor").onkeyup = checkUpdate;
-window.addEventListener("beforeunload", function (e) {
-  alert();
-})
 
 window.onbeforeunload = function (event) {
   var message = 'Sure you want to leave?';
@@ -211,6 +245,7 @@ window.onbeforeunload = function (event) {
 
 var updateTime = 0;
 function checkUpdate(e){
+  trackChanges();
   if (e != "close"){            //wait for 5 seconds before last update
     var currentTime = Math.floor( new Date().getTime()/1000 );
     if (updateTime - currentTime < -5){
@@ -223,14 +258,108 @@ function checkUpdate(e){
       updateServer();
   }
 }
-var dd = document.getElementById("editor");
 
 function updateServer(){
-  dd.textarea();
-
+  diffs = [];
+  content = dd.textarea.val();
+  //console.log(content);
   console.log("updating...");
-  content = $("#editor").val();
+
   $.post( document.URL, {
-    "content":  content
+    "content": content
   });
+}
+
+var cursor_pos;
+var text = "";
+var lc_time = "";
+var diffs = [];
+var diff = [];
+var operation;
+var prev_operation;
+
+var editor_div = document.getElementById("editor");
+
+$("#editor").focus(function(){
+  cursor_pos = getCaret(editor_div);
+  console.log(cursor_pos);
+});
+
+function trackChanges(){
+  
+  now_text = dd.textarea.val();
+  now_cursor_pos = getCaret(editor_div);
+
+  d_tx = now_text.length - text.length;
+  d_cr = now_cursor_pos - cursor_pos;
+  //console.log(d_tx, d_cr);
+  if (now_text == text)
+    change_type = "no change";
+  else{
+    var currentTime = Math.floor( new Date().getTime()/1000 );
+    d_t = currentTime - lc_time;
+    console.log(d_tx, d_cr);
+    opChange();
+    if ( d_tx > 0 && d_cr > 0  && d_tx == d_cr ) {              
+        //insert operation at continous location
+      change_type = "ins";
+      diff = ["ins", cursor_pos, now_text.substr(cursor_pos, d_cr), d_t];
+      operation = 1;
+    } else if ( ( d_tx < 0 ^ d_cr < 0 ) && d_tx != d_cr ) {
+      //insert at different location
+      operation = 2;
+    } else if ( (d_tx < 0 && d_cr < 0 ) && d_tx != d_cr ) {
+      //delete at continous location
+      operation = 3;
+      change_type = "del";
+      diff = ["del", cursor_pos, -d_tx, d_t];
+    } else{
+      //delete at different location
+      operation = 4;
+      change_type = "del";
+      diff = ["del", cursor_pos, -d_tx, d_t];
+    }
+    console.log(diff);
+    lc_time = currentTime;
+  }
+
+  function opChange(){
+    console.log(cursor_pos);
+    if ( ( operation != prev_operation && (operation==1 || operation==3)) || operation==2 || operation==4 ){
+      diffs.push(diff);
+      diff = [];
+      cursor_pos = getCaret(editor_div);
+      text = now_text;
+    }
+    prev_operation = operation;
+  }
+
+}
+
+function getCaret(el) {
+  if (el.selectionStart)
+    return el.selectionStart; 
+  else if (document.selection) { 
+    el.focus(); 
+    var r = document.selection.createRange(); 
+    if (r == null) { 
+      return 0; 
+    } 
+    var re = el.createTextRange(), 
+    rc = re.duplicate(); 
+    re.moveToBookmark(r.getBookmark()); 
+    rc.setEndPoint('EndToStart', re); 
+
+    var add_newlines = 0;
+    for (var i=0; i<rc.text.length; i++) {
+      if (rc.text.substr(i, 2) == '\r\n') {
+        add_newlines += 2;
+        i++;
+      }
+    }
+    //return rc.text.length + add_newlines;
+    //Substract the no. of lines
+    return rc.text.length - add_newlines; 
+  }  
+  return 0; 
 }
--- public/javascripts/temp.js
+++ public/javascripts/temp.js
@@ -1,43 +1,203 @@
+// saved buffer
+var sbuffer = new Buffer('', 0);
+var buffers = [sbuffer];
+var deft = document.getElementById("deft");
+var current = 0;
+var commits = 0;
 
-document.getElementById("editor").onkeyup = checkUpdate;
-window.addEventListener("beforeunload", function (e) {
-  alert();
-})
+var text = "";
+var cursor_pos = 0;
+var change_type = "";
+var changes = 0;
+var d_tx = 0;
+var d_cr = 0;
 
-window.onbeforeunload = function (event) {
-  var message = 'Sure you want to leave?';
-  if (typeof event == 'undefined') {
-    event = window.event;
-  }
-  if (event) {
-    event.returnValue = message;
-  }
-  return message;
-  checkUpdate("close");
+var lc_time = -1;
+var diffs = [];
+
+var special_cmd = false;
+
+function getTime() {
+    return (new Date).getTime();
 }
 
-var updateTime = 0;
-function checkUpdate(e){
-  if (e != "close"){            //wait for 5 seconds before last update
-    var currentTime = Math.floor( new Date().getTime()/1000 );
-    if (updateTime - currentTime < -5){
-      updateTime = currentTime;
-      //update server
-      updateServer();
+function playback() {
+    playback_buffer = new Buffer('', 0);
+    playback_buffer.set();
+    if (diffs.length > 0) {
+        r_playback();
     }
-  } else{                     //disregard time on close and update server
-      updateTime = currentTime;
-      updateServer();
-  }
 }
-var dd = document.getElementById("editor");
 
-function updateServer(){
-  dd.textarea();
-
-  console.log("updating...");
-  content = $("#editor").val();
-  $.post( document.URL, {
-    "content":  content
-  });
+function r_playback() {
+    diff = diffs.shift();
+    if (diff[0] == "ins") {
+        pb_text = deft.value;
+        start = pb_text.slice(0, diff[1]);
+        end = pb_text.slice(diff[1]);
+        deft.value = start + diff[2] + end;
+    }
+    if (diff[0] == "del") {
+        pb_text = deft.value;
+        start = pb_text.slice(0, diff[1]);
+        end = pb_text.slice(diff[1] + diff[2]);
+        deft.value = start + end;
+    }
+    if (diffs.length > 0) {
+        window.setTimeout(r_playback, diffs[0][3]);
+    }
 }
+
+
+track_changes = function() {
+    if (lc_time < 0) { lc_time = getTime(); }
+
+    now_text = deft.value;
+    now_cursor_pos = getCaret(deft);
+
+    d_tx = now_text.length - text.length;
+    d_cr = now_cursor_pos - cursor_pos;
+
+    if (now_text == text || special_cmd) {
+        change_type = "no change";
+        special_cmd = false;
+    } else {
+        change_time = getTime(); 
+        d_t = change_time - lc_time;
+        changes++;
+
+        if (d_tx == d_cr) {
+            if (now_text.length > text.length) {
+                change_type = "simple insert";
+                diff = ["ins", cursor_pos, now_text.substr(cursor_pos, d_cr), d_t];
+                diffs.push(diff);
+            } else {
+                change_type = "simple delete";
+                diff = ["del", cursor_pos, -d_tx, d_t];
+                diffs.push(diff);
+            }
+        } else {
+            change_type = "composite";
+            diff = ["del", cursor_pos, d_cr - d_tx, d_t];
+            diffs.push(diff);
+            if (d_cr > 0) {
+                diff = ["ins", cursor_pos, now_text.substr(cursor_pos, d_cr), 10];
+                diffs.push(diff);
+            }
+        }
+
+        text = now_text;
+        lc_time = change_time;
+    }
+
+    cursor_pos = getCaret(deft);
+    status();
+};
+
+
+deft.onmouseup = track_changes;
+deft.onmousemove = track_changes;
+deft.onkeyup = track_changes;
+
+//deft.oninput = track_changes;
+
+function Buffer(text, cursor) {
+    this.text = text; this.cursor = cursor;
+}
+
+Buffer.prototype.set = function() {
+    deft.value = this.text;
+    setCaret(deft, this.cursor);
+}
+
+Buffer.prototype.toString = function() {
+    return this.cursor + ":" + this.text;
+}
+
+function getBuffer() {
+    return new Buffer(deft.value.replace(/ +/g, ' '), getCaret(deft));
+}
+
+function commit() {
+    sbuffer = getBuffer();
+    buffers = [sbuffer];
+    current = 0;
+    commits++;
+}
+
+function save() {
+    buffer = getBuffer();
+    buffers[current] = buffer;
+}
+
+function do_cmd(f) {
+    save();
+
+    special_cmd = true;
+    change_time = getTime(); 
+    d_t = change_time - lc_time;
+    diffs.push(["del", 0, deft.value.length, d_t]);
+
+    f();
+
+    diffs.push(["ins", 0, deft.value, d_t]);
+    lc_time = change_time;
+}
+
+function scratch() {
+    do_cmd(function() {
+        current = buffers.length;
+        buffers.push(sbuffer);
+        sbuffer.set();
+    });
+}
+
+function right() {
+    do_cmd(function() {
+        current = (current + 1) % buffers.length;
+        buffers[current].set();
+    });
+}
+
+function left() {
+    do_cmd(function() {
+        current = current == 0 ? (buffers.length - 1) : current - 1;
+        buffers[current].set();
+    });
+}
+
+function status() {
+    status2();
+}
+
+function status1() {
+    var html = "Draft: <b>" + (current + 1) + "</b>" + "/" + buffers.length;
+    html += " - Commit: <b>" + commits + "</b>";
+    document.getElementById("buffers").innerHTML = html;
+}
+
+function status2() {
+    html = "cursorpos: " + cursor_pos + " change: " + change_type + " d_tx: " + d_tx + " d_cr: " + d_cr;
+    document.getElementById("buffers").innerHTML = html;
+}
+
+function bind(sc, f) {
+    Mousetrap.bind(sc, function(e) {
+        if (e.preventDefault) { e.preventDefault(); } else { e.returnValue = false; }
+        f();
+        status();
+    });
+}
+
+//bind('ctrl+h', function() { left(); });
+bind('ctrl+l', function() { right(); });
+bind('ctrl+h', function() { left(); });
+//bind('ctrl+l', function() { right(); });
+
+//bind('alt+left', function() { left(); });
+//bind('alt+right', function() { right(); });
+//bind('alt+up', function() { commit(); });
+//bind('alt+down', function() { scratch(); });
+
+bind('ctrl+s', function() { commit(); });
+bind('ctrl+space', function() { scratch(); });
